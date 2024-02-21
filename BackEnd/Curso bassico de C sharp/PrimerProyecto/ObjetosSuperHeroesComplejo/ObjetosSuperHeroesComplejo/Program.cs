f94b0e42f3f5b42951979d3f4b5468ad98442b50
santiago a <santiagoalvarezporras@gmail.com>
2024-02-21 11:34:10 -0500

    practica 4

---
+++ BackEnd/Curso bassico de C sharp/PrimerProyecto/ObjetosSuperHeroesComplejo/ObjetosSuperHeroesComplejo/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var poderVolar = new SuperPoder();
+poderVolar.Nombre = "Volar";
+poderVolar.Descripcion = "Capacidad para volar y planear en el aire";
+poderVolar.Nivel = NivelPoder.NivelDos;
+
+var superFuerza = new SuperPoder();
+superFuerza.Nombre = "Super fuerza";
+superFuerza.Descripcion = "Posee una fuerza descomunal";
+superFuerza.Nivel = NivelPoder.NivelTres;
+
+
+
+var superman = new SuperHeroe();
+
+superman.Id = 1;
+superman.Nombre = "Superman";
+superman.IdentidadSecreta = "Clark Kent";
+superman.Ciudad = "Metropolis";
+superman.PuedeVolar = true;
+
+List<SuperPoder> poderesSuperman = new List<SuperPoder>();
+poderesSuperman.Add(poderVolar);
+poderesSuperman.Add(superFuerza);
+
+superman.SuperPoderes = poderesSuperman;
+
+class SuperHeroe
+{
+    public int Id;
+    public string Nombre;
+    public string IdentidadSecreta;
+    public string Ciudad;
+    public List<SuperPoder> SuperPoderes;
+    public bool PuedeVolar;
+}
+
+class SuperPoder
+{
+    public string Nombre;
+    public string Descripcion;
+    public NivelPoder Nivel;
+}
+
+enum NivelPoder
+{
+    NivelUno,
+    NivelDos,
+    NivelTres
+}
+
